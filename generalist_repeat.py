da747be48cacdf48665d184d4bb4bf4c1843dcd1
Gid-Git <gidon.quint@ziggo.nl>
2023-10-20 10:43:05 +0200

    some runs and generalist_repeat for comma

--- generalist_repeat.py
+++ generalist_repeat.py
@@ -5,8 +5,8 @@ parameters = {
     'experiment_name': "variable_enemies_commma",
     'enemies': [1, 4, 6, 7],
     'population_size': 100,
-    'generations': 3,
-    'mutation_rate': 0.1,
+    'generations': 30,
+    'mutation_rate': 0.054128345043897554,
     'crossover_rate': 1,
     'mode': "train",
     'n_hidden_neurons': 10,
@@ -14,10 +14,10 @@ parameters = {
     'dom_l': -1,
     'dom_u': 1,
     'speed': "fastest", 
-    'number_of_crossovers': 3,
+    'number_of_crossovers': 2,
     'n_elitism': 2,
     'k_tournament': 4,
-    'k_tournament_final_linear_increase_factor': 4,   
+    'k_tournament_final_linear_increase_factor': 2,   
     'alpha': 0.5,
     'type_of_selection_pressure': 'exponential',
     'enemy_threshold': 10,
@@ -28,9 +28,9 @@ parameters = {
 # for running and saving the code 10 times
 def run_single_experiment(i):
     number = i + 1
-    unique_experiment_name = f"experiment_testing_{number}"
+    unique_experiment_name = f"experiment_adaptive_comma_{number}"
     parameters["experiment_name"] = unique_experiment_name
     run_evoman(**parameters)
 
 if __name__ == "__main__":
-    Parallel(n_jobs=-1)(delayed(run_single_experiment)(i) for i in range(3))
+    Parallel(n_jobs=-1)(delayed(run_single_experiment)(i) for i in range(10))
